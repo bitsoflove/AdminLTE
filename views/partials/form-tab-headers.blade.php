990cc0b0ad8c97623a0cb0542b763de4bb97250f
Pepijn Olivier <olivier.pepijn@gmail.com>
2016-01-12 15:12:06 +0100

    update form tab headers
    Signed-off-by: Pepijn Olivier <olivier.pepijn@gmail.com>

--- views/partials/form-tab-headers.blade.php
+++ views/partials/form-tab-headers.blade.php
@@ -6,18 +6,33 @@
 
 <?php $prefix = isset($prefix) ? $prefix."_" : ""; ?>
 
-<?php if (count(LaravelLocalization::getSupportedLocales()) > 1): ?>
-<ul class="nav nav-tabs">
-    <?php $i = 0; ?>
-    <?php foreach (LaravelLocalization::getSupportedLocales() as $locale => $language): ?>
-        <?php $i ++; ?>
-        <?php $class = ''; ?>
-        <?php foreach ($errors->getMessages() as $field => $messages): ?>
-            <?php if (substr($field, 0, strpos($field, ".")) == $locale) $class = 'error' ?>
-        <?php endforeach ?>
-        <li class="{{ App::getLocale() == $locale ? 'active' : '' }} {{ $class }}">
-            <a href="#tab_{{ $prefix.$i }}" data-toggle="tab">{{ trans('core::core.tab.'. strtolower($language['name'])) }}</a>
-        </li>
-    <?php endforeach; ?>
-</ul>
-<?php endif; ?>
+
+@if(!is_module_enabled('Site'))
+    <?php if (count(LaravelLocalization::getSupportedLocales()) > 1): ?>
+    <ul class="nav nav-tabs">
+        <?php $i = 0; ?>
+        <?php foreach (LaravelLocalization::getSupportedLocales() as $locale => $language): ?>
+            <?php $i ++; ?>
+            <?php $class = ''; ?>
+            <?php foreach ($errors->getMessages() as $field => $messages): ?>
+                <?php if (substr($field, 0, strpos($field, ".")) == $locale) $class = 'error' ?>
+            <?php endforeach ?>
+            <li class="{{ App::getLocale() == $locale ? 'active' : '' }} {{ $class }}">
+                <a href="#tab_{{ $prefix.$i }}" data-toggle="tab">{{ trans('core::core.tab.'. strtolower($language['name'])) }}</a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+    <?php endif; ?>
+@else
+    <ul class="nav nav-tabs">
+        @foreach(\Site::current()->siteLocales->lists('title', 'locale')->toArray() as $locale => $title)
+            <?php $class = ''; ?>
+            <?php foreach ($errors->getMessages() as $field => $messages): ?>
+                    <?php if (substr($field, 0, strpos($field, ".")) == $locale) $class = 'error' ?>
+                <?php endforeach ?>
+            <li class="{{ App::getLocale() == $locale ? 'active' : '' }} {{ $class }}">
+                <a href="#tab_{{ $prefix.$locale }}" data-toggle="tab">{{ trans('core::core.tab.'.  $locale) }}</a>
+            </li>
+        @endforeach
+    </ul>
+@endif
