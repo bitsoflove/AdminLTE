bcbdafa088a41f4a2a08059cc13e30ccf73a183f
Pepijn Olivier <olivier.pepijn@gmail.com>
2016-01-18 11:21:56 +0100

    added backend page layout for errors (without styling)
    Signed-off-by: Pepijn Olivier <olivier.pepijn@gmail.com>

---
+++ views/front/layouts/page.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+
+    {{-- The purpose for this layout is to serve the BACKEND error views in project/app/resources/views/errors --}}
+
+
+    @yield('title')
+    @yield('content')
+
+</body>
+</html>
