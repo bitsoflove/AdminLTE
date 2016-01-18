06673f6abbbc9807b33eace5321b11a3f49fc392
RienMoeyaert <rien@bitsoflove.be>
2016-01-18 16:41:01 +0100

    Update master layout

--- views/layouts/master.blade.php
+++ views/layouts/master.blade.php
@@ -5,7 +5,11 @@
     <meta charset="UTF-8">
     <title>
         @section('title')
-        {{ Setting::get('core::site-name') }} | Admin
+            @if( isset( \Modules\Site\Facades\Site::currentLocale()->title ))
+                {{ \Modules\Site\Facades\Site::currentLocale()->title }} | Admin
+            @else
+                {{ Setting::get('core::site-name') }} | Admin
+            @endif
         @show
     </title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
@@ -15,14 +19,14 @@
     {!! Theme::script('vendor/jquery/jquery.min.js') !!}
     @include('partials.asgard-globals')
     @section('styles')
-    @show
+        @show
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-    <![endif]-->
+                <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+        <![endif]-->
 </head>
 <body class="{{ config('asgard.core.core.skin', 'skin-blue') }}" style="padding-bottom: 0 !important;">
 <div class="wrapper">
@@ -30,6 +34,10 @@
         <a href="{{ URL::route('dashboard.index') }}" class="logo">
             <?php if (isset($sitename)): ?>
             {{ $sitename }}
+            <?php endif; ?>
+
+            <?php if (isset(\Modules\Site\Facades\Site::currentLocale()->title)): ?>
+            {{ \Modules\Site\Facades\Site::currentLocale()->title }}
             <?php endif; ?>
         </a>
         @include('partials.top-nav')
@@ -56,14 +64,14 @@
     <script src="{{ URL::asset($js) }}" type="text/javascript"></script>
 @endforeach
 <?php if (is_module_enabled('Notification')): ?>
-    <script src="https://js.pusher.com/3.0/pusher.min.js"></script>
-    <script src="{{ Module::asset('notification:js/pusherNotifications.js') }}"></script>
-    <script>
-        $(".notifications-list").pusherNotifications({
-            pusherKey: '{{ env('PUSHER_KEY') }}',
-            loggedInUserId: {{ $currentUser->id }}
-        });
-    </script>
+<script src="https://js.pusher.com/3.0/pusher.min.js"></script>
+<script src="{{ Module::asset('notification:js/pusherNotifications.js') }}"></script>
+<script>
+    $(".notifications-list").pusherNotifications({
+        pusherKey: '{{ env('PUSHER_KEY') }}',
+        loggedInUserId: {{ $currentUser->id }}
+    });
+</script>
 <?php endif; ?>
 
 @section('scripts')
